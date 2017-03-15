21fe2271f1b24e88e872397464d5290bd9ef30fc
David Sheng <shengji250@gmail.com>
2017-03-14 23:03:53 -0400

    Re-added MergeSort with timer
    Damage control? Dunno.

---
+++ src/MergeSort.java
@@ -0,0 +1,101 @@
+import java.io.*;
+import java.util.*;
+
+public class MergeSort
+{
+	public static final int SIZE = 50;          // Size of array to be sorted
+	private static int[] values = new int[SIZE];  // Values to be sorted
+ 
+	private static void initValues()
+	// Initializes the values array with random integers from 0 to 99
+	{
+		Random rand = new Random(560);
+		for (int index = 0; index < SIZE; index++)
+			values[index] = Math.abs(rand.nextInt()) % 100;
+	}
+ 
+	public static boolean isSorted()
+	// Determine whether the array values are sorted
+	{
+		boolean sorted = true;
+		for (int i = 0; i < values.length - 1; i++)
+			if (values[i] > values[i + 1])
+				sorted = false;
+		return sorted;   
+	}
+ 
+	public static void swap(int index1, int index2)
+	// Swaps the integers at locations index1 and index2 of array values
+	// Precondition:  index1 and index2 are less than size
+	{
+		int temp = values[index1];
+		values[index1] = values[index2];
+		values[index2] = temp;
+	}
+ 	
+ 	public static void mergeSort(int startIndex, int endIndex)
+ 	{
+ 		if (startIndex < endIndex)
+        {
+            int index = (startIndex + endIndex) / 2;
+            mergeSort(startIndex, index);
+            mergeSort(index + 1, endIndex);
+            merge(startIndex, index, index + 1, endIndex);
+        }
+ 	}
+ 	
+ 	public static void merge(int leftFirst, int leftLast, int rightFirst, int rightLast)
+ 	{
+ 		int[] temp = new int[SIZE];
+ 		int saveFirst = leftFirst;
+ 		int index = leftFirst;
+ 		
+ 		while (leftLast - leftFirst >= 0 && rightLast - rightFirst >= 0)
+ 		{
+ 			if (values[leftFirst] < values[rightFirst]) 
+ 			{
+ 				temp[index] = values[leftFirst];
+ 				leftFirst++;
+ 			}
+ 			else
+ 			{
+ 				temp[index] = values[rightFirst];
+ 				rightFirst++;
+ 			}
+ 			index++;
+ 		}
+ 		
+ 		while (leftFirst <= leftLast)
+ 	    {
+ 	        temp[index] = values[leftFirst];
+ 	        leftFirst++;
+ 	        index++;
+ 	    }
+ 	   
+ 		while (rightFirst <= rightLast)
+ 	    {
+ 	        temp[index] = values[rightFirst];
+ 	        rightFirst++;
+ 	        index++;
+ 	    }
+ 		
+ 		for (int i = saveFirst; i <= rightLast; i++)
+ 	        values[i] = temp[i];
+ 	}
+
+	public static void main(String[] args) throws IOException
+	// Tests the other methods of the Sorts class
+	{
+		long startTime = 0;
+    	long endTime = 0;
+    	for (int i = 0; i < 30; i++)
+    	{
+    		initValues();
+    		startTime = System.nanoTime();
+    		mergeSort(0, SIZE - 1);
+    		endTime += System.nanoTime() - startTime;
+    	}
+    	endTime /= 30;
+    	System.out.println(endTime + " nanoseconds");
+	}   
+}
